c04c6e26590fdf423d95cd256203c1b4f215261e
Alex <32961755+howe-oh@users.noreply.github.com>
2020-06-02 00:26:05 +0100

    Added contact info & testing PHP

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 const appendToLinks = () => {
-  try {
-    //append "index" to home links
-    home = document.querySelectorAll(".homeLink");
-    for (var i = 0; i < home.length; i++) home[i].href += "index";
+  //append "index" to home links
+  home = document.querySelectorAll(".homeLink");
+  for (var i = 0; i < home.length; i++) home[i].href += "index";
 
-    //append ".html" to all links
-    links = document.getElementsByTagName("a");
-    for (let link of links) link.href += ".html";
-  } catch (error) {
-    console.log("There was an issue with appending to links:", error);
-  }
+  //append ".html" to all links
+  l = document.querySelectorAll(".nav-link, .dropdown-item, .navbar-brand");
+  for (var i = 0; i < l.length; i++) l[i].href += ".html";
 };
 
 window.onload = () => {
