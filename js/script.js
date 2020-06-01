0797cba7bc59ad3eed1e222d97cb72678d5f0a7e
Alex <32961755+howe-oh@users.noreply.github.com>
2020-06-01 18:22:02 +0100

    Removed '.html' reference at end of all sites

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-function f() {
-  if (window.location.protocol != "file:") return;
-  links = document.getElementsByTagName("a");
-  for (let link of links) link.href += ".html";
-}
+const appendToLinks = () => {
+  try {
+    //append "index" to home links
+    home = document.querySelectorAll(".homeLink");
+    for (var i = 0; i < home.length; i++) home[i].href += "index";
 
-window.onload = f;
+    //append ".html" to all links
+    links = document.getElementsByTagName("a");
+    for (let link of links) link.href += ".html";
+  } catch (error) {
+    console.log("There was an issue with appending to links:", error);
+  }
+};
+
+window.onload = () => {
+  //If locally hosted, append relevant text to links to ensure functionality
+  if (window.location.protocol == "file:") appendToLinks();
+};
