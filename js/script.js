d2f39bd036f6293e9b003c01342a2ea99d451eae
Alex <32961755+howe-oh@users.noreply.github.com>
2020-07-17 12:33:19 +0100

    fixed links & made URLs look a lil more website-like

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-const appendToLinks = () => {
+const appendIfLocal = () => {
+  //If locally hosted, append relevant text to links to ensure functionality
+  if (window.location.protocol != "file:") return;
+
   //append "index" to home links
   home = document.querySelectorAll(".homeLink");
   for (var i = 0; i < home.length; i++) home[i].href += "index";
 
-  //append ".html" to all links
-  l = document.querySelectorAll(".nav-link, .dropdown-item, .navbar-brand");
+  //append ".html" to all internal links
+  l = document.querySelectorAll(
+    ".nav-link, .dropdown-item, .navbar-brand, .internal"
+  );
   for (var i = 0; i < l.length; i++) l[i].href += ".html";
 };
 
 window.onload = () => {
-  //If locally hosted, append relevant text to links to ensure functionality
-  if (window.location.protocol == "file:") appendToLinks();
+  appendIfLocal();
 };
