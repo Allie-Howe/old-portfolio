cba54ccc6816a26f5ba5143694ea5f4082a7cf46
Allie Howe <32961755+howe-oh@users.noreply.github.com>
2022-06-05 14:30:11 +0100

    Refactoring - simplified geo to iso functions

--- projects/isometric/p5scripts.js
+++ projects/isometric/p5scripts.js
@@ -4,14 +4,15 @@ const h = 5
 const w = h;
 const squareSize = Math.min(window.innerHeight, window.innerWidth)/10
 const IS_MOBILE = window.innerWidth < 600 ? true : false;
-let STARTING_SCALE = IS_MOBILE ? .25 : 1;
+const STARTING_SCALE = IS_MOBILE ? .25 : 1;
 let SCALE = STARTING_SCALE;
 let clrs;
 let cubeImg;
 let gui, guiVars = {
   cubeSpacing: 0,
   amplitude: 50,
-  hoverMode: false
+  speed: 2,
+  hoverMode: true
 }
 
 function preload() {
@@ -19,6 +20,7 @@ function preload() {
   gui = new dat.GUI()
   gui.add(guiVars, "cubeSpacing", 0, 100)
   gui.add(guiVars, "amplitude", 0, 100)
+  gui.add(guiVars, "speed", 0, 5)
   gui.add(guiVars, "hoverMode")
 }
 
@@ -30,10 +32,8 @@ function setup() {
     bg: color(0),
     fg: color(255,255,255)
   }
-  a = 0.5 * cubeImg.width;
-  b = -a;
-  c = 0.25 * cubeImg.height;
-  d = c;
+  adjustedH = cubeImg.height + guiVars.cubeSpacing
+  adjustedW = cubeImg.width + guiVars.cubeSpacing
 
   background(clrs.bg);
   fill(clrs.fg);
@@ -48,37 +48,53 @@ function draw() {
   drawGrid();
 }
 
+// Grid
 function drawGrid() {
-  // let offset = 0;
   for (let i = -Math.floor(h/2); i <= Math.floor(h/2); i++) {
     for (let j = -Math.floor(w/2); j <= Math.floor(w/2); j++) {
-      // offset+=.05;
       doGridItem(i, j);
     }
   }
 }
 
-function doGridItem(i, j, offset) {
-  // normalise displacement to amount of cubes
-  const displacement = sin(((millis()/500) * (i+j))) * guiVars.amplitude;
+function doGridItem(i, j) {
   push();
   const [geoX, geoY] = getGeoFromIso([j, i]);
   translate(geoX, geoY);
-  if (guiVars.hoverMode) {
-    if (-i === mousePos[1] && -j === mousePos[0]) translate(0, -10);
-  } else translate(0, displacement);
+  applyDisplacement(i, j);
   image(cubeImg, 0, 0);
   pop();
 }
 
+// Displacement
+function calculateDisplacement(i, j) {
+    // TODO: normalise displacement to amount of cubes
+    const SPEED_NORMALISER = 1000
+
+    const speed = guiVars.speed/SPEED_NORMALISER;
+
+    return sin((millis()*speed) * (i+j)) * guiVars.amplitude
+}
+
+function applyDisplacement(i, j) {
+  if (guiVars.hoverMode) {
+    if (-i === mousePos[1] && -j === mousePos[0]) translate(0, -10);
+  } else translate(0, calculateDisplacement(i, j));
+}
+
+// Geometry helper functions
+let adjustedW, adjustedH;
+
+// Vector functions
 const i = () => [1, 0.5];
 const j = () => [-1, 0.5];
-const adjustGeoToCubeWidth = ([ix, iy], [jx, jy]) => [
-  (cubeImg.width + guiVars.cubeSpacing)/2 * (ix + jx),
-  (cubeImg.height + guiVars.cubeSpacing)/2 * (iy + jy)
-];
+const iInv = () => [1/adjustedW, -1/adjustedW]
+const jInv = () => [2/adjustedH, 2/adjustedH]
 
-const normaliseGeo = (n) => (2*n)/((cubeImg.width + guiVars.cubeSpacing)/2)
+const adjustGeoToCubeWidth = ([ix, iy], [jx, jy]) => [
+  adjustedW/2 * (ix + jx),
+  adjustedH/2 * (iy + jy)
+];
 
 function getGeoFromIso([isoX, isoY]) {
   const ix = i().map(v => v * isoX);
@@ -86,8 +102,13 @@ function getGeoFromIso([isoX, isoY]) {
   return adjustGeoToCubeWidth(ix, jy);
 }
 
+function getIsoFromGeo([geoX, geoY]) {
+  const [x1, y1] = iInv().map(v => v * geoX);
+  const [x2, y2] = jInv().map(v => v * geoY);
+  return [x1 + x2, y1 + y2];
+}
 
-// EVENTS
+// Events
 let oldY;
 function mouseDragged() {
   const diff = oldY - window.mouseY;
@@ -96,22 +117,9 @@ function mouseDragged() {
   SCALE+=(diff/100)
 }
 
-let mousePos = []
+let mousePos = [];
 function mouseMoved() {
   const XPos = width/2 - mouseX;
-  const YPos = height/2 - mouseY ;
+  const YPos = height/2 - mouseY;
   mousePos = getIsoFromGeo([XPos, YPos]).map(floor);
-}
-
-const inverseThing = (n) => n/((a*d)-(b*c))
-
-let a, b, c, d;
-function getIsoFromGeo([geoX, geoY]) {
-const newI = [inverseThing(d), inverseThing(-c)];
-const newJ = [inverseThing(-b), inverseThing(a)];
-
-const [x1, y1] = newI.map(v => v * geoX)
-const [x2, y2] = newJ.map(v => v * geoY)
-
-return [x1 + x2, y1 + y2]
 }
