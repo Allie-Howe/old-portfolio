a77959ca92a973caaef4493dcd7475858e870340
Allie Howe <32961755+howe-oh@users.noreply.github.com>
2022-06-05 14:44:28 +0100

    removed drag to scale

--- projects/isometric/src/events.js
+++ projects/isometric/src/events.js
@@ -1,12 +1,4 @@
 // Events
-let oldY;
-function mouseDragged() {
-  const diff = oldY - window.mouseY;
-  oldY = window.mouseY;
-  if (SCALE <= STARTING_SCALE && diff < 0) return
-  SCALE+=(diff/100)
-}
-
 let mousePos = [];
 function mouseMoved() {
   const XPos = width/2 - mouseX;
