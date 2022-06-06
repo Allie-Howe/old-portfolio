4f30f98e56d4b5cc2d6ff745dcb852a57f6a1efe
Allie Howe <32961755+Allie-Howe@users.noreply.github.com>
2022-06-06 02:39:30 +0100

    Update events.js

--- projects/isometric/src/events.js
+++ projects/isometric/src/events.js
@@ -2,8 +2,8 @@
 let DISP_HEIGHT = -10
 let mousePos = [];
 function mouseMoved() {
-  const XPos = (width/2 - mouseX) * SCALE;
-  const YPos = (height/2 - mouseY) * SCALE;
+  const XPos = (width/2 - mouseX) / SCALE;
+  const YPos = (height/2 - mouseY) / SCALE;
   mousePos = getIsoFromGeo([XPos, YPos]).map(floor);
 }
 
