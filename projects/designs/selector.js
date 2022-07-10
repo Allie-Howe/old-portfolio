583a8209a118ce46e446e667e0d49fb75e848e63
Allie Howe <32961755+howe-oh@users.noreply.github.com>
2022-07-10 02:35:08 +0100

    Added gif path array

---
+++ projects/designs/selector.js
@@ -0,0 +1,10 @@
+const PATH = "./assets";
+const paths = [
+  `${PATH}/C+L_new.gif`,
+  `${PATH}/checkers.gif`,
+  `${PATH}/ContCirc_new.gif`,
+  `${PATH}/love.gif`,
+  `${PATH}/snake.gif`,
+  `${PATH}/triangles.gif`,
+  // `${PATH}/recursive_squares.mp4`,
+]
