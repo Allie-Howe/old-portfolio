f48f266185d75b43d1d6010fc137c1949153dc86
Allie Howe <32961755+howe-oh@users.noreply.github.com>
2022-07-10 02:46:29 +0100

    Added supplying varying src to image

--- projects/designs/selector.js
+++ projects/designs/selector.js
@@ -8,3 +8,14 @@ const paths = [
   `${PATH}/triangles.gif`,
   // `${PATH}/recursive_squares.mp4`,
 ]
+let pathIndex = 0;
+
+const changePath = () => document.querySelector("#gifArea").src = paths[pathIndex];
+
+window.onload = () => {
+  changePath();
+  document.querySelector("#gifArea").onclick = () => {
+    pathIndex = (pathIndex + 1) % paths.length;
+    changePath();
+  }
+}
